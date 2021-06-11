3b199016ecaa2b4eca39de92b99fa52b6e9f146f
CTPache <33907485+CTPache@users.noreply.github.com>
2021-06-11 12:10:46 +0200

    Hotfix del parche.
    Faltaba un directorio en el layeredFS

--- ParcheaJuego.bat
+++ ParcheaJuego.bat
@@ -3,10 +3,15 @@ chcp 65001
 
 .\res\ctrtool.exe --contents=contents "%~1"
 .\res\ctrtool.exe --romfs=romfs.bin .\contents.0000.00000000
-.\res\xdelta3.exe -d -s romfs.bin res\patch.xdelta romfsPatched.bin
+
+.\res\xdelta3.exe -d -f -s romfs.bin res\patch.xdelta romfsPatched.bin
+
+pause
 
 set /p id="¿Versión americana (U) o europea (E)? "
+
 If %id%==E (set d=00040000000F1400) ELSE (set d=00040000000F1E00)
+
 mkdir %d%
 .\res\ctrtool.exe --romfsdir=%d%\romfs romfsPatched.bin
 
