8319b2bfff0b40083769c6cbd9a4bf858e8fbbfc
CTPache <33907485+CTPache@users.noreply.github.com>
2021-06-11 16:51:45 +0200

    Update ParcheaJuego.bat

--- ParcheaJuego.bat
+++ ParcheaJuego.bat
@@ -1,22 +1,16 @@
-@echo off
 chcp 65001
 
-.\res\ctrtool.exe --contents=contents "%~1"
-.\res\ctrtool.exe --romfs=romfs.bin .\contents.0000.00000000
+.\res\ctrtool.exe --contents=contents "%~1" || pause
+.\res\ctrtool.exe --romfs=romfs.bin .\contents.0000.00000000 || pause
+.\res\xdelta3.exe -d -s romfs.bin res\patch.xdelta romfsPatched.bin || pause
 
-.\res\xdelta3.exe -d -f -s romfs.bin res\patch.xdelta romfsPatched.bin
 
-pause
-
-set /p id="¿Versión americana (U) o europea (E)? "
-
-If %id%==E (set d=00040000000F1400) ELSE (set d=00040000000F1E00)
-
-mkdir %d%
-.\res\ctrtool.exe --romfsdir=%d%\romfs romfsPatched.bin
+set /p id="¿Versión americana (U) o europea (E)? " || pause
+If %id%==E (set d=00040000000F1E00) ELSE (set d=00040000000F1400) || pause
+mkdir %d% || pause
+echo %d% || pause
+.\res\ctrtool.exe --romfsdir=%d%\romfs romfsPatched.bin || pause
 
 del romfs.bin
 del romfsPatched.bin
 del contents.000*
-
-pause
